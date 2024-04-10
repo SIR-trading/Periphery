2ce7d8e5d2cbda4793b27fc4ae01ea0e76a18fcc
Codonyat <nil.garcia@gmail.com>
2024-04-10 17:32:16 +0200

    Added some code in the Assistan mint function

--- script/Assistant.sol
+++ script/Assistant.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.0;
 
 import {Vault} from "core/Vault.sol";
+import {VaultStructs} from "core/libraries/VaultStructs.sol";
+import {TransferHelper} from "core/libraries/TransferHelper.sol";
 
-contract Assistant {}
+contract Assistant {
+    Vault public vault;
+
+    constructor(address vault_) {
+        vault = Vault(vault_);
+    }
+
+    function mint(bool isAPE, uint256 amount, address debtToken, address collateralToken, int8 leverageTier) external {
+        // ADD MIN AMOUNT? THIS COULD BE A PROTECTION AGAINST RUGGY TOKENS AND, ALTHOUGH LESS DANGEROUS, PRICE CHANGES
+
+        // Transfer collateral from user to vault
+        TransferHelper.safeTransferFrom(collateralToken, msg.sender, address(vault), amount);
+
+        // Mint TEA or APE
+        vault.mint(isAPE, VaultStructs.VaultParameters(debtToken, collateralToken, leverageTier));
+    }
+}
