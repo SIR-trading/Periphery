52e7cd682175a29af7901124713254c7e9f3037b
Codonyat <nil.garcia@gmail.com>
2024-08-30 14:15:15 +0200

    feat: updated sripts for deploying on Sepolia

--- script/DeployPeriphery.s.sol
+++ script/DeployPeriphery.s.sol
@@ -7,16 +7,28 @@ import {Assistant} from "src/Assistant.sol";
 import {Addresses} from "core/libraries/Addresses.sol";
 
 contract DeployPeriphery is Script {
+    uint256 deployerPrivateKey;
+
     address public vault;
     bytes32 public hashCreationCodeAPE;
 
     function setUp() public {
+        if (block.chainid == 1) {
+            deployerPrivateKey = vm.envUint("TARP_TESTNET_PRIVATE_KEY");
+        } else if (block.chainid == 11155111) {
+            deployerPrivateKey = vm.envUint("SEPOLIA_DEPLOYER_PRIVATE_KEY");
+        } else {
+            revert("Network not supported");
+        }
+
         vault = vm.envAddress("VAULT");
         hashCreationCodeAPE = vm.envBytes32("HASH_CREATION_CODE_APE");
     }
 
+    /** cli for local testnet:  forge script script/DeployPeriphery.s.sol --rpc-url tarp_testnet --broadcast --legacy
+        cli for Sepolia:        forge script script/DeployPeriphery.s.sol --rpc-url sepolia --chain sepolia --broadcast
+     */
     function run() public {
-        uint256 deployerPrivateKey = vm.envUint("TARP_TESTNET_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
         // Deploy oracle
