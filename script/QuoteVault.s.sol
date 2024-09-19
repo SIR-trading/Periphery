d7d0656dd1ce014bbfe6d8b95a89b7c65b0a1f90
Codonyat <nil.garcia@gmail.com>
2024-09-19 17:08:25 +0200

    feat: script to quote vaults

---
+++ script/QuoteVault.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+import {IVault} from "core/interfaces/IVault.sol";
+import {Assistant} from "src/Assistant.sol";
+import {SirStructs} from "core/libraries/SirStructs.sol";
+
+contract QuoteVault is Script {
+    uint256 deployerPrivateKey;
+
+    IVault public vault;
+    Assistant public assistant;
+
+    function setUp() public {
+        if (block.chainid == 1) {
+            deployerPrivateKey = vm.envUint("TARP_TESTNET_PRIVATE_KEY");
+        } else if (block.chainid == 11155111) {
+            deployerPrivateKey = vm.envUint("SEPOLIA_DEPLOYER_PRIVATE_KEY");
+        } else {
+            revert("Network not supported");
+        }
+
+        vault = IVault(vm.envAddress("VAULT"));
+        assistant = Assistant(vm.envAddress("ASSISTANT"));
+    }
+
+    /** cli for local testnet:  forge script script/QuoteVault.s.sol --rpc-url tarp_testnet --broadcast --legacy
+        cli for Sepolia:        forge script script/QuoteVault.s.sol --rpc-url sepolia --chain sepolia --broadcast
+     */
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
+
+        SirStructs.VaultParameters memory vaultParams = vault.paramsById(1);
+
+        // Quote vault id 1
+        uint256 amountTokens = assistant.quoteMint(true, vaultParams, 1e18);
+        console.log("Minting expects: ", amountTokens);
+
+        vm.stopBroadcast();
+    }
+}
